c04d692a494843cc1429925ea06af52b2aa1cf75
Gabriel Spranger <gabrielspranger@MacBook-Pro-de-Gabriel.local>
2021-05-10 18:50:32 -0500

    insert training rows and testing rows in two different lists done, ready to start regression

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
-from math import e
+from math import e, floor, ceil
 from datetime import datetime
+
+p: int = 6 # grado del polinomio de nuestro modelo de regresión no lineal
 
 DATE = 0; CONFIRMED = 1; DEATHS = 2; ZONE = 3; ALTITUDE = 4; LAT = 5; LON = 6;
 
-training_file = 'covid.csv'
+filename = 'covid.csv'
 y_training_ds = []
 x_training_ds = []
 x_testing_ds = []
 y_testing_ds = []
 
-with open(training_file) as csv_file:
+all_rows: int = -1
+
+with open(filename) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-    # n = len(list(csv_reader))
-    # print('rows: ', n)
-    # TODO: 70% pal training y 30% pal testing
-    idx = 0
+    # all_rows = len(list(csv_reader))
+    all_rows = 9787
+    training_rows = floor(all_rows*0.7)
+    testing_rows = ceil(all_rows*0.3)
+    print('total rows: ', all_rows)
+    print('training rows: ', training_rows)
+    print('testing rows: ', testing_rows)
+    idx: int = 0
     for row in csv_reader:
-        if idx != 0:
-          if idx == 5:
-            break
+        if idx != 0: # ignore header
+          # if idx == 5: # only first 5 rows
+          #   break
           print(idx, row)
           # print(row[DATE])
-          marca_de_tiempo = datetime.strptime(str(row[DATE]), "%Y-%m-%d").timestamp()
+          marca_de_tiempo: int = datetime.strptime(str(row[DATE]), "%Y-%m-%d").timestamp()
           # print(marca_de_tiempo)
-          zona = row[ZONE]
+          zona: str = row[ZONE]
           if zona == 'ZONA NORTE':
             zona = 1.0
           elif zona == 'ZONA CENTRO':
@@ -36,10 +44,24 @@ with open(training_file) as csv_file:
           else:
             print('unkown zona')
             exit(0)
-          x_training_ds.append([float(marca_de_tiempo), float(zona), float(row[LAT]), float(row[LON])])
-          y_training_ds.append(float(row[DEATHS]))
+          new_x = [float(marca_de_tiempo), float(zona), float(row[LAT]), float(row[LON])]
+          new_y = float(row[DEATHS])
+          if idx <= training_rows:
+            print('training row')
+            x_training_ds.append(new_x)
+            y_training_ds.append(new_y)
+          else:
+            print('testing row')
+            x_testing_ds.append(new_x)
+            y_testing_ds.append(new_y)
+          
+          # print rows
           # print(x_training_ds[idx-1])
           # print(y_training_ds[idx-1])
         idx += 1
+
+print('total rows after insertion: ', all_rows)
+print('training rows after insertion: ', len(x_training_ds))
+print('testing rows after insertion: ', len(x_testing_ds))
 
 w = np.random.rand(p)
