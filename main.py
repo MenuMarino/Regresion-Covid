da63dba59bfe5ce9f50743868de7cdd5eb2c8331
pastoruri <40170714+pastoruri@users.noreply.github.com>
2021-05-16 15:37:10 -0500

    Adagrad

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import csv
 import matplotlib.pyplot as plt
 from math import e, floor, ceil
 from datetime import datetime
-from numpy.random import MT19937
-from numpy.random import RandomState, SeedSequence
-from time import time
+
 p: int = 4 # grado del polinomio de nuestro modelo de regresión no lineal
 
 REGION = 0; REGION_ORIG = 1; DATE = 2; CONFIRMED = 3; DEATHS = 4
@@ -55,12 +53,12 @@ print('total rows after insertion: ', n)
 print('training rows after insertion: ', len(x_training_ds))
 print('testing rows after insertion: ', len(x_testing_ds))
 
-#rs = RandomState(MT19937(SeedSequence(123456789))); rs.rand(4)
-#w = rs.rand(p)
-w = np.random.rand(p)
-print("TERMINOS: ",w)
-landa = 0.015
-alfa = 0.0005
+rs = RandomState(MT19937(SeedSequence(123456789))); rs.rand(4)
+w = rs.rand(p)
+print(w)
+exit(0)
+landa = 1.5
+alfa = 0.0025
 gamma = 0.9
 v = [0] * p
 print(w)
@@ -105,31 +103,23 @@ errores = []
 
 def test():
   k = 1
-  while (k < 100):
+  while (k < 200):
     unidades.append(k)
-    grads = [derivada_l2_regularizada(y_training_ds, h, landa, w, j, x_training_ds) for j in range(p)]
+    grads = [derivada_l2(y_training_ds, h, landa, w, j, x_training_ds) for j in range(p)]
     for i in range(p):
-      w[i] = w[i] - alfa*grads[i]
+        v[i] = gamma*v[i] + alfa*grads[i]
+        w[i] = w[i] - v[i]
     err = mse(y_training_ds, h)
     errores.append(err)
-    #print(err)
+    print(err)
     k += 1
 
-
-start_time = time()
 test()
-elapsed_time = time() - start_time
-
-print('lambda: ', landa)
-print('alfa: ', alfa)
-print('time: ', elapsed_time)
 print('error: ', mse(y_training_ds, h))
 # print(unidades)
 # print(errores)
 plt.plot(unidades, errores)
 
-
-'''
 for i in range(q-1):
     row = x_testing_ds[i]
     true = y_testing_ds[i]
@@ -139,4 +129,4 @@ for i in range(q-1):
    # 1.48 landa 1 alfa 0.003
    # 1.47 landa 1.5  """"
    # 1.466 landa 1.5 0.0025
-'''
+
